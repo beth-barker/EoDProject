ef1e22c23abc41c27ac2e4735ff8c442a2b4c0db
Beth Barker <barker.k.elizabeth@gmail.com>
2023-03-16 17:40:22 -0600

    March 16th

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const newTaskForm = document.querySelector('form')
+
 let task1 = {name: 'Sweep the floor', priority: 'Low', status: false}
 
 let task2 = {name: 'Get groceries', priority: 'Medium', status: true}
@@ -8,15 +10,39 @@ let tasks = [task1, task2, task3]
 
 console.log(tasks)
 
-const addTask = (event) => {
-    event.preventDefault()
+const buildTasks = (tasks) => {
+  let tasksDisplay = document.getElementById('task-display')
+  tasksDisplay.innerHTML = ''
 
-    const name = document.querySelector("#new-task-name")
-    const priority = document.querySelector("new-task-priority")
+  tasks.forEach((task) => {
+    let newTask = document.createElement('div')
+    newTask.classList.add('task')
 
-    const newTask = {name: name.value, priority: priority.value, status: false}
+    newTask.innerHTML = `<input type="checkbox" class="task-completed"/>
+    <p class="task-name">${task.name}</p>
+    <p class="task-priority">${task.priority}</p>
+    <img
+      class='trash-can'
+      src='https://www.freeiconspng.com/thumbs/trash-can-icon/trash-can-icon-26.png'
+      alt='trash'
+    />`
 
-    tasks.push(newTask)
-    console.log(tasks)
-
+    tasksDisplay.appendChild(newTask)
+  })
 }
+
+const addTask = (event) => {
+  event.preventDefault()
+
+  const name = document.querySelector("#new-task-name")
+  const priority = document.querySelector("#new-task-priority")
+
+  const newTask = {name: name.value, priority: priority.value, status: false}
+
+  tasks.push(newTask)
+  buildTasks(tasks)
+}
+
+newTaskForm.addEventListener('submit', addTask)
+
+buildTasks(tasks)
